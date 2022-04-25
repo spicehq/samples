2e68ae1caf47b3c52f84d457ac60f59551a2f7a7
Luke Kim <80174+lukekim@users.noreply.github.com>
2022-04-25 16:52:57 +0900

    Add API_KEY

--- javascript/snippets/ethersjs/main.js
+++ javascript/snippets/ethersjs/main.js
@@ -1,5 +1,5 @@
 import { SpiceProvider } from './spice-provider.js';
 
-const provider = new SpiceProvider();
+const provider = new SpiceProvider('homestead', 'API_KEY');
 
 console.log('The latest block number is', await provider.getBlockNumber());
