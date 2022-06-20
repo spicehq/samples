3b9ba734d82fe18f04beeb22117c93a8a6284fe1
Luke Kim <80174+lukekim@users.noreply.github.com>
2022-06-20 15:50:31 +0900

    Updates

--- javascript/snippets/spicejs/main.js
+++ javascript/snippets/spicejs/main.js
@@ -8,4 +8,4 @@ const spiceClient = new SpiceClient('API_KEY');
 const arrowTable = await spiceClient.query(
   'SELECT number,hash FROM eth.recent_blocks ORDER BY number DESC LIMIT 10;'
 );
-console.log(arrowTable.toJSON());
+console.table(arrowTable.toArray());
