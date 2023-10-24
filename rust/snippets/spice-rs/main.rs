d76f77d80cb15df804786e6d3574e9af83b5adcb
Sevenannn <qianqliu@uw.edu>
2023-10-24 06:50:14 -0700

    Update Rust smaple

--- rust/snippets/spice-rs/main.rs
+++ rust/snippets/spice-rs/main.rs
@@ -1,11 +1,11 @@
 use futures::stream::StreamExt;
 use spice_rs::*;
 
-let mut spice_client = new_spice_client(api_key).await.expect("Failed to new spice client");
+let mut spice_client = new_spice_client(api_key).await;
 
 match spice_client.query(
-    "SELECT number, \"timestamp\", base_fee_per_gas, base_fee_per_gas / 1e9 AS base_fee_per_gas_gwei FROM eth.recent_blocks limit 10".to_string(),
-    None).await {
+    "SELECT number, \"timestamp\", base_fee_per_gas, base_fee_per_gas / 1e9 AS base_fee_per_gas_gwei FROM eth.recent_blocks limit 10".to_string()
+    ).await {
         Ok(mut flight_data_stream) => {
               // Read back RecordBatches
             while let Some(batch) = flight_data_stream.next().await {
